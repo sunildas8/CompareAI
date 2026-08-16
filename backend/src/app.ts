373ca76e1867140c18646141ab30eb6513922510
Sunil Das <95643764+sunildas1@users.noreply.github.com>
2026-08-16 21:32:38 +0600

    Add CORS support and update API endpoints for improved functionality

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,30 @@
 import express from 'express';
-import useGraph from './services/graph.ai.service.js';
+import runGraph from './services/graph.ai.service.js'
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: process.env.PORT,
+    methods: ['GET', 'POST'],
+    credentials: true,
+}));
 
-app.get('/health', (req, res) => {
+app.get('/', (req, res) => {
     res.status(200).json({ status: 'ok' });
 });
 
-app.post('/', async (req, res) => {
-   const result = await useGraph("write an factorial function in javascript");
-   res.json(result);
+app.post('/invoke', async (req, res) => {
+    const { input } = req.body;
+    const result = await runGraph(input);
+    res.status(200).json({
+        message: 'Graph AI invoked successfully',
+        success: true,
+        result,
+    });
 });
 
 export default app;
